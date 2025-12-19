e6d7bc4e629c8951467eff9b38f42d1acbedcdb3
jomiy191-arch <jomiy191@gmail.com>
2025-12-19 20:23:11 +0500

    Some changes

--- js/main.js
+++ js/main.js
@@ -14,3 +14,36 @@ function updateClock() {
 
 setInterval(updateClock, 1000);
 updateClock();
+const display = document.getElementById('display');
+
+
+function appendValue(value) {
+if (display.innerText === '0') {
+display.innerText = value;
+} else {
+display.innerText += value;
+}
+}
+
+
+function clearDisplay() {
+display.innerText = '0';
+}
+
+
+function deleteLast() {
+if (display.innerText.length > 1) {
+display.innerText = display.innerText.slice(0, -1);
+} else {
+display.innerText = '0';
+}
+}
+
+
+function calculate() {
+try {
+display.innerText = eval(display.innerText);
+} catch (e) {
+display.innerText = 'Error';
+}
+}
